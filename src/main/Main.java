d4e9c16f79e908490391bdb580217e3251b61663
Shivam Baghel <shivamunlimited@gmail.com>
2020-03-26 20:17:03 +0530

    Yeah

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,24 +11,24 @@ import tree.MyBinarySearchTree;
 public class Main {
     public static void main(String[] args) {
         MyBinarySearchTree mytree=new MyBinarySearchTree();
-        mytree.insert(50);
-        mytree.insert(30);
         mytree.insert(20);
-        mytree.insert(40);
+        mytree.insert(30);
+        mytree.insert(10);
         mytree.insert(70);
+        mytree.insert(50);
         mytree.insert(60);
         mytree.insert(80);
-//        int elementToSearch=89;
-//        System.out.println(elementToSearch +" present ? : "+mytree.search(elementToSearch));
-//        elementToSearch=60;
-//        System.out.println(elementToSearch +" present ? : "+mytree.search(elementToSearch));
-//        System.out.println("In Order Traversal of tree :");
-//        mytree.traverseInOrder();
-//        System.out.println("Pre Order Traversal of tree :");
-//        mytree.traversePreOrder();
-//        System.out.println("Post Order Traversal of tree :");
-//        mytree.traversePostOrder();
-//        System.out.println("Total no of nodes : "+mytree.count());
+        int elementToSearch=100;
+        System.out.println(elementToSearch +" present ? : "+mytree.search(elementToSearch));
+        elementToSearch=60;
+        System.out.println(elementToSearch +" present ? : "+mytree.search(elementToSearch));
+        System.out.println("In Order Traversal of tree :");
+        mytree.traverseInOrder();
+        System.out.println("Pre Order Traversal of tree :");
+        mytree.traversePreOrder();
+        System.out.println("Post Order Traversal of tree :");
+        mytree.traversePostOrder();
+        System.out.println("Total no of nodes : "+mytree.count());
     }
 }
 
