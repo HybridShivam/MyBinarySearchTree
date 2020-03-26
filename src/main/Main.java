47713dfe633e18ebd6e610fe93279545b74a525e
Shivam Baghel <shivamunlimited@gmail.com>
2020-03-26 20:14:54 +0530

    Yeah

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,17 +18,17 @@ public class Main {
         mytree.insert(70);
         mytree.insert(60);
         mytree.insert(80);
-        int elementToSearch=89;
-        System.out.println(elementToSearch +" present ? : "+mytree.search(elementToSearch));
-        elementToSearch=60;
-        System.out.println(elementToSearch +" present ? : "+mytree.search(elementToSearch));
-        System.out.println("In Order Traversal of tree :");
-        mytree.traverseInOrder();
-        System.out.println("Pre Order Traversal of tree :");
-        mytree.traversePreOrder();
-        System.out.println("Post Order Traversal of tree :");
-        mytree.traversePostOrder();
-        System.out.println("Total no of nodes : "+mytree.count());
+//        int elementToSearch=89;
+//        System.out.println(elementToSearch +" present ? : "+mytree.search(elementToSearch));
+//        elementToSearch=60;
+//        System.out.println(elementToSearch +" present ? : "+mytree.search(elementToSearch));
+//        System.out.println("In Order Traversal of tree :");
+//        mytree.traverseInOrder();
+//        System.out.println("Pre Order Traversal of tree :");
+//        mytree.traversePreOrder();
+//        System.out.println("Post Order Traversal of tree :");
+//        mytree.traversePostOrder();
+//        System.out.println("Total no of nodes : "+mytree.count());
     }
 }
 
